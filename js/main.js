7ab9ff1274a68b5cc556013b70040004521d45d0
itsnabeel <itsnabeel@gmail.com>
2019-03-29 20:35:45 +0400

    new structure is done

--- js/main.js
+++ js/main.js
@@ -1,111 +1,21 @@
 "use strict";
 (function () {
-    var scrolling = false, expanded = false, current_section = 0, circlesShown = false;
-    var $body = $('body'), $win = $(window), $projects = $('.projects'), $resume_details = $('.resume_details_block'),
-        $to_top_btn = $('.back-to-top'), $fp_nav = $('.full-page-nav-container');
+    var scrolling = false,
+        expanded = false,
+        current_section = 0,
+        circlesShown = false;
+    var $body = $('body'),
+        $win = $(window),
+        $projects = $('.projects'),
+        $resume_details = $('.resume_details_block'),
+        $to_top_btn = $('.back-to-top'),
+        $fp_nav = $('.full-page-nav-container');
 
     $(document).on('ready', function () {
-        console.log("%c Contact: itsnabeel@gmail.com","color: #FFFFFF; background: #CF0A2C; font-size: x-large");
+        console.log("%c Contact: itsnabeel@gmail.com", "color: #FFFFFF; background: #CF0A2C; font-size: x-large");
 
-    $body.addClass('loaded');
+        $body.addClass('loaded');
         $body.css('overflow-x', 'hidden');
-        // load projects for small screens
-        $('.load_projects').on('click', function () {
-            if ($projects.data('height') === undefined || $projects.data('height') === null) {
-                $projects.data('height', $projects.height() + 30); // 30px padding of project
-                $projects.css({'max-height': 'none'});
-                $(this).html("Load less");
-            } else {
-                $projects.css({'max-height': $projects.data('height')});
-                $projects.data('height', null);
-                $(this).html("Load more");
-                $('html, body').scrollTop($projects.offset().top);
-            }
-            return false;
-        });
-
-        // load projects button for large screens
-        $('#load_projects').on('click', function () {
-            if ($win.width() > 991) {
-                if ($projects.data('height') === undefined || $projects.data('height') === null) {
-                    $projects.data('height', $projects.height() + 30); // 30px padding of project
-                    $projects.css({'max-height': 'none'});
-                    $projects.closest('.vcenter').addClass('block');
-
-                    scrollPage(null, 70, function () {
-                        $projects.addClass('expanded');
-                    });
-                    expanded = true;
-                    $(this).fadeOut(1000);
-                }
-            }
-            return false;
-        });
-
-        // more details button on Resume section for Mobile
-        $('.more-resume').on('click', function () {
-            $resume_details.toggle();
-
-            scrollPage(null, $resume_details.offset().top);
-            $win.scroll(); // to trigger appear event thus animating progress bars
-            var val = $(this).html();
-            $(this).html((val === 'More Details') ? "Less Details" : "More Details");
-            return false;
-        });
-
-        // more details button on Resume section for Desktop
-        $('#more-resume').on('click', function () {
-            if ($win.width() > 991) {
-                $resume_details.show();
-
-                scrollPage(null, $resume_details.offset().top, function () {
-                    $resume_details.addClass('expanded');
-                });
-
-                $('#education').addClass('dim');
-                $body.css('overflow-x', 'hidden');
-                expanded = true;
-                $(this).fadeOut(1000);
-            }
-            return false;
-        });
-
-
-        $body.on('mousewheel', function (e) {
-            // horizontal scrolling will be applied on screens larger than 991px
-            if ($win.width() > 991) {
-                var total_sections = $('.wrap > section').length - 1;
-
-                // already in scrolling state don't take any action
-                if (scrolling)
-                    return false;
-
-                // if reading resume details then let user scroll vertically
-                if (expanded) {
-                    return true;
-                }
-
-                //check the direction of scroll
-                if (e.deltaY > 0) {
-                    if (current_section > 0)
-                        current_section--;
-                } else {
-                    if (current_section < total_sections)
-                        current_section++;
-                }
-                scrolling = true;
-
-                // move page horizontally based on scroll direction
-                scrollPage($('.wrap >section:eq(' + current_section + ')').offset().left, null);
-
-                // wait to prevent unnecessary scroll (better UX)
-                window.setTimeout(function () {
-                    scrolling = false;
-                }, 1200);
-
-                return false;
-            }
-        });
 
 
         $body.on('appear', '.sec-title', function (e, $affected) {
@@ -136,8 +46,14 @@
                     text: {
                         autoStyleContainer: false
                     },
-                    from: {color: $color, width: 4},
-                    to: {color: $color, width: 4},
+                    from: {
+                        color: $color,
+                        width: 4
+                    },
+                    to: {
+                        color: $color,
+                        width: 4
+                    },
                     // Set default step function for all animate calls
                     step: function (state, circle) {
                         circle.path.setAttribute('stroke', state.color);
@@ -153,7 +69,7 @@
                     }
                 });
 
-                bar.animate($(this).data('value') / 100);  // Number from 0.0 to 1.0
+                bar.animate($(this).data('value') / 100); // Number from 0.0 to 1.0
             });
 
 
@@ -165,7 +81,10 @@
                     color: $color,
                     trailColor: $('bod').css('backgroundColor'),
                     trailWidth: 0,
-                    svgStyle: {width: '100%', height: '10px'},
+                    svgStyle: {
+                        width: '100%',
+                        height: '10px'
+                    },
                     text: {
                         style: {
                             // Text color.
@@ -180,15 +99,19 @@
                         },
                         autoStyleContainer: false
                     },
-                    from: {color: '#FFEA82'},
-                    to: {color: '#ED6A5A'},
+                    from: {
+                        color: '#FFEA82'
+                    },
+                    to: {
+                        color: '#ED6A5A'
+                    },
                     step: function (sate, bar) {
                         bar.setText(Math.round(bar.value() * 100) + ' %');
                         bar.text.style.right = Math.round((.82 - bar.value()) * 100) + '%';
                     }
                 });
 
-                bar.animate($(this).data('value') / 100);  // Number from 0.0 to 1.0
+                bar.animate($(this).data('value') / 100); // Number from 0.0 to 1.0
             });
 
         });
@@ -226,7 +149,7 @@
                         $('#load_projects').focus();
                     },
                     afterClose: function () {
-                        $('html, body').scrollLeft($('.wrap >section:eq(' + current_section + ')').offset().left);
+                        // $('html, body').scrollLeft($('.wrap >section:eq(' + current_section + ')').offset().left);
                     },
 
                     close: function () {
@@ -245,108 +168,17 @@
         });
 
 
-        $(".typed").typed({
-            stringsElement: $('#typed-title'),
-            typeSpeed: 100,
-            callback: function () {
-                $(".title .typed-cursor").remove();
-                $(".sub-typed").typed({
-                    stringsElement: $('#typed-sub-title'),
-                    typeSpeed: 70,
-                    loop: true,
-                    backDelay: 1000,
-                    loopCount: 3, //false for infinite
-                    backspace: function (curString, curStrPos) {
-                        // check string array position
-                        // on the first string, only delete one word
-                        // the stopNum actually represents the amount of chars to
-                        // keep in the current string. In my case it's 3.
-                        if (self.arrayPos === 1) {
-                            self.stopNum = 3;
-                        }
-                        //every other time, delete the whole typed string
-                        else {
-                            self.stopNum = 0;
-                        }
-                    }
-                });
-            }
-        });
-
         $('.nav a').on('click', function () {
             $('.navbar-toggle').click();
         });
 
         var
-            speed = 700,   // animation speed
-            masonryOptions = {         // initial masonry options
-                itemSelector: '.project',
-                animate: true,
-                percentPosition: true,
-                columnWidth: '.project-sizer',
+            speed = 700; // animation speed
 
-                animationOptions: {
-                    duration: speed,
-                    queue: true
-                }
-            };
         var $filter = $('#filters');
 
-        // run on window.load so we can capture any incoming hashes
-        $win.on('load', function () {
-            // run masonry on start-up to capture all the boxes we'll need
-            $projects.masonry(masonryOptions);
-            if (window.location.hash) {
-                var possibleFilterClass = window.location.hash.replace('#', '');
-                switch (possibleFilterClass) {
-                    // if the hash matches the following words
-                    case 'web' :
-                    case 'branding' :
-                    case 'advertisement' :
-                        masonryOptions.animate = false;
-                        $projects.find('.project').not('.' + possibleFilterClass)
-                            .toggleClass('invis').hide();
-                        $projects.masonry(masonryOptions);
 
-                        $filter.find('a').removeClass("active");
-                        $filter.find("[data-filter='" + possibleFilterClass + "']").addClass('active');
-                        break;
-                }
-            }
-        });
-
-
-        // Projects Filter script
-        $filter.on('click', 'a', function () {
-            var
-                type = $(this).data('filter'),
-                filterClass = '.' + type;
-
-            if (filterClass === '.all') {
-                // show all hidden boxes
-                $projects.find('.invis')
-                    .toggleClass('invis').fadeIn(200, function () {
-                    $projects.masonry();
-                });
-            } else {
-                // hide visible boxes
-                $projects.find('.project').not(filterClass).not('.invis')
-                    .toggleClass('invis').fadeOut(400);
-                // show hidden boxes
-                $projects.find(filterClass + '.invis')
-                    .toggleClass('invis').fadeIn(200);
-            }
-            window.setTimeout(function () {
-                $projects.masonry({animate: true});
-            }, 500);
-
-            // set hash in URL
-            window.location.hash = type;
-            $filter.find('a').removeClass("active");
-            $(this).addClass("active");
-            return false;
-        });
-
+     
         //Smooth page scroll
         $('.navbar-nav').on('click', 'a', function (event) {
             var anchor = '#' + $(this).attr('href').split('#')[1];
@@ -364,30 +196,6 @@
         } else {
             $fp_nav.removeClass('sticky');
             $to_top_btn.removeClass('visible');
-        }
-
-        if ($resume_details.hasClass('expanded') && $win.scrollTop() < 300 && $win.width() > 767) {
-
-            scrollPage(null, 0, function () {
-                $resume_details.removeClass('expanded').hide();
-                $('#education').removeClass('dim');
-            }, 50);
-
-            expanded = false;
-            $('#more-resume').fadeIn(1000);
-        }
-        if ($projects.hasClass('expanded') && $win.scrollTop() < 50 && $win.width() > 767) {
-            scrollPage(null, 0, function () {
-                $projects.removeClass('expanded');
-            }, 50);
-
-            $projects.css({'max-height': $projects.data('height')});
-            $projects.data('height', null);
-            $projects.closest('.vcenter').removeClass('block');
-
-            expanded = false;
-            $('#load_projects').fadeIn(1000);
-
         }
     });
 
